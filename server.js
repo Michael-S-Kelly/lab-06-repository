d606c5d53d02076562d673f34e9c32ab717f3126
Michael-S-Kelly <mike.s.kelly@icloud.com>
2018-12-04 10:55:07 -0800

    removed front-end/updated server

--- server.js
+++ server.js
@@ -15,31 +15,31 @@ const app = express();
 app.use(cors());
 
 //test route for checking to ensure API works
-app.get('/testroute', function(request, response) {
-  let animal = { type: 'turtle', name: 'tim' };
-  response.json(animal);
-});
-
-// app.get('/location', (request, response) => {
-//   console.log('my request object:', request);
-//   const locationData = searchToLatLong(request.query.data);
-//   response.send(locationData);
+// app.get('/testroute', function(request, response) {
+//   let animal = { type: 'turtle', name: 'tim' };
+//   response.json(animal);
 // });
+
+app.get('/location', (request, response) => {
+  console.log('my request object:', request);
+  const locationData = searchToLatLong(request.query.data);
+  response.send(locationData);
+});
 
 // helper function goes here
 
-// function searchToLatLong(query) {
-//   const geoData = require('./data/geo.json');
-//   const location = new Location(geoData.results[0]);
-//   location.search_query = query;
-//   return location;
-// }
+function searchToLatLong(query) {
+  const geoData = require('./data/geo.json');
+  const location = new Location(geoData.results[0]);
+  location.search_query = query;
+  return location;
+}
 
-// function Location(data) {
-//   this.formatted_query = data.formatted_address;
-//   this.latitude = data.geometry.location.lat;
-//   this.longitude = data.geometry.location.lng;
-// }
+function Location(data) {
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+}
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
